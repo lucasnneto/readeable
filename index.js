d4233ff86a6c12d9960cff97ce1215b9952889f6
Lucas Neto <lucasn-oliveira@hotmail.com>
2022-06-10 12:06:59 -0300

    API

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
 const PizZip = require("pizzip");
 const Docxtemplater = require("docxtemplater");
+require("dotenv").config();
 
 const fs = require("fs");
 const path = require("path");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-// Load the docx file as binary content
-const content = fs.readFileSync(path.resolve(__dirname, "temp.docx"), "binary");
+app.get("/", (req, res) => {
+  // Load the docx file as binary content
+  const content = fs.readFileSync(
+    path.resolve(__dirname, "temp.docx"),
+    "binary"
+  );
 
-const zip = new PizZip(content);
+  const zip = new PizZip(content);
 
-const doc = new Docxtemplater(zip, {
-  paragraphLoop: true,
-  linebreaks: true,
+  const doc = new Docxtemplater(zip, {
+    paragraphLoop: true,
+    linebreaks: true,
+  });
+
+  // Render the document (Replace {first_name} by John, {last_name} by Doe, ...)
+  doc.render({
+    nome: "John",
+    cpf: "123456789",
+  });
+  console.log("docx render");
+  const buf = doc.getZip().generate({
+    type: "nodebuffer",
+    // compression: DEFLATE adds a compression step.
+    // For a 50MB output document, expect 500ms additional CPU time
+    compression: "DEFLATE",
+  });
+
+  // buf is a nodejs Buffer, you can either write it to a
+  // file or res.send it with express for example.
+  fs.writeFileSync(path.resolve(__dirname, "output.docx"), buf);
+  console.log("docx criado");
+  const { convertWordFiles } = require("convert-multiple-files");
+
+  const docToPdf = async (filePath, outputDir) =>
+    await convertWordFiles(filePath, "pdf", outputDir);
+
+  docToPdf(path.resolve(__dirname, "output.docx"), path.resolve(__dirname));
+  console.log("pdf criado");
+  return res.send("finish");
 });
 
-// Render the document (Replace {first_name} by John, {last_name} by Doe, ...)
-doc.render({
-  nome: "John",
-  cpf: "123456789",
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
-
-const buf = doc.getZip().generate({
-  type: "nodebuffer",
-  // compression: DEFLATE adds a compression step.
-  // For a 50MB output document, expect 500ms additional CPU time
-  compression: "DEFLATE",
-});
-
-// buf is a nodejs Buffer, you can either write it to a
-// file or res.send it with express for example.
-fs.writeFileSync(path.resolve(__dirname, "output.docx"), buf);
-const { convertWordFiles } = require("convert-multiple-files");
-
-const docToPdf = async (filePath, outputDir) =>
-  await convertWordFiles(filePath, "pdf", outputDir);
-
-docToPdf(path.resolve(__dirname, "output.docx"), path.resolve(__dirname));
